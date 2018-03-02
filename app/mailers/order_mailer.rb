4e91dbede44d5bb51c1c6f3164f7a9146d0a79aa
DercilioFontes <dercilioafontes@gmail.com>
2018-03-02 19:33:45 +0000

    Complete implamentation of the feature/order-receipt and tested.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,10 +4,8 @@ class OrderMailer < ApplicationMailer
   def order_email(order)
     @order = order
     @url = "orders/#{order.id}"
-    delivery_options = { address: 'smtp://0.0.0.0:1025'}
 
     mail(to: order.email, 
-         subject: "Jungle e-Receipt - Order ##{order.id}",
-         delivery_method_options: delivery_options)
+         subject: "Jungle e-Receipt - Order ##{order.id}")
   end
 end
