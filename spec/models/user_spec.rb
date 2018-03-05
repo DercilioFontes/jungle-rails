ae1f029f2ac10c059d6f9d2a296330a1acbe254f
DercilioFontes <dercilioafontes@gmail.com>
2018-03-05 23:14:41 +0000

    Implemented ste 1 and step 2 RSpec for user and tested.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  it "must not be created with different password and password_corfimation" do
+    @user = User.create(first_name: 'Sylvia', last_name: 'Almeida', email: 'sylvia@gmail.com', password: 'KkKk', password_confirmation: 'KtKk')
+
+    expect(@user.errors.full_messages()).to eq ["Password confirmation doesn't match Password"]
+  end
+
+  it "must be created with same password and password_corfimation" do
+    @user = User.create(first_name: 'Sylvia', last_name: 'Almeida', email: 'sylvia@gmail.com', password: 'KkKk', password_confirmation: 'KkKk')
+
+    expect(User.find(@user.id)).not_to be_nil
+  end
+
+  it "must not be created with a email that have already been saved in DB, even case sensitive" do
+    @user1 = User.create(first_name: 'Sylvia', last_name: 'Almeida', email: 'sylvia@gmail.com', password: 'KkKk', password_confirmation: 'KkKk')
+    @user2 = User.create(first_name: 'Sylvia', last_name: 'Fontes', email: 'SYLVIA@gmail.com', password: 'PpPp', password_confirmation: 'PpPp')
+
+    expect(@user2.errors.full_messages()).to eq ["Email has already been taken"]
+  end
+
+  it "must not be created without first name" do
+    @user = User.create(first_name: nil, last_name: 'Almeida', email: 'sylvia@gmail.com', password: 'KkKk', password_confirmation: 'KkKk')
+
+    expect(@user.errors.full_messages()).to eq ["First name can't be blank"]
+  end
+
+  it "must not be created without last name" do
+    @user = User.create(first_name: 'Sylvia', last_name: nil, email: 'sylvia@gmail.com', password: 'KkKk', password_confirmation: 'KkKk')
+
+    expect(@user.errors.full_messages()).to eq ["Last name can't be blank"]
+  end
+
+  it "must not be created without email" do
+    @user = User.create(first_name: 'Sylvia', last_name: 'Almeida', email: nil, password: 'KkKk', password_confirmation: 'KkKk')
+
+    expect(@user.errors.full_messages()).to eq ["Email can't be blank"]
+  end
+
+end
