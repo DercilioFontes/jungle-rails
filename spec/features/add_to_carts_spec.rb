3d872760b6e50c48250f5cef8fa28b45c5721a14
DercilioFontes <dercilioafontes@gmail.com>
2018-03-07 01:11:56 +0000

    Implemented feature/testing-add-to-cart.

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  
+  scenario "Visitor can navigate from the home page to the product detail page by clicking on a product" do
+
+    # ACT
+    visit root_path
+
+    first('article.product').find_link('Add').click
+
+    # DEBUG / VERIFY
+    save_screenshot 'jungle.png'
+
+    expect(page).to have_content 'My Cart (1)'
+  end
+
+
+end
